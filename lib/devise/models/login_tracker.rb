395e74e06597623af4026acfc3911cc2dc48dbe5
Jiří Zajpt <jzajpt@blueberry.cz>
2013-09-13 18:26:57 +0200

    A bit of inline documentation

--- lib/devise/models/login_tracker.rb
+++ lib/devise/models/login_tracker.rb
@@ -2,10 +2,19 @@ module Devise
   module Models
     module LoginTracker
 
+      # Creates a new login record based on information in provided request
+      # object.
+      #
+      # @param [ActionDispatch::Request] request HTTP request which was used
+      # to login
+      # @return [UserLogin]
       def mark_login!(request)
         login_class.create(attrs_for_login(request))
       end
 
+      # Marks the time when user has logged out on given login record ID.
+      #
+      # @param [Fixnum, String] login_record_id ID of login record.
       def mark_logout!(login_record_id)
         login_record = login_class.find(login_record_id)
         login_record.update_column :signed_out_at, Time.now
