473c7d419d2da6e6c090ed71f891e2ab3a10e069
Jiří Zajpt <jzajpt@blueberry.cz>
2013-09-13 18:24:19 +0200

    Set signed_out_at upon logout

--- lib/devise/hooks/login_tracker.rb
+++ lib/devise/hooks/login_tracker.rb
@@ -1,4 +1,16 @@
 Warden::Manager.after_authentication do |record, warden, opts|
-  record.mark_login!(warden.request) if record.respond_to?(:mark_login!)
+  if record.respond_to?(:mark_login!)
+    login_record = record.mark_login!(warden.request)
+    scope = opts[:scope]
+    warden.session["#{scope}.login_id"] = login_record.id
+  end
 end
 
+
+Warden::Manager.before_logout do |record, warden, opts|
+  if record.respond_to?(:mark_logout!)
+    scope = opts[:scope]
+    login_record_id = warden.session["#{scope}.login_id"]
+    record.mark_logout!(login_record_id) if login_record_id
+  end
+end
--- lib/devise/models/login_tracker.rb
+++ lib/devise/models/login_tracker.rb
@@ -3,14 +3,23 @@ module Devise
     module LoginTracker
 
       def mark_login!(request)
-        UserLogin.create(attrs_for_user_login(request))
+        login_class.create(attrs_for_login(request))
+      end
+
+      def mark_logout!(login_record_id)
+        login_record = login_class.find(login_record_id)
+        login_record.update_column :signed_out_at, Time.now
       end
 
       protected
 
-      def attrs_for_user_login(request)
+      def attrs_for_login(request)
         { user_id: id, ip_address: request.remote_ip,
           user_agent: request.user_agent, signed_in_at: Time.now }
+      end
+
+      def login_class
+        "#{self.class.name}Login".safe_constantize
       end
 
     end
